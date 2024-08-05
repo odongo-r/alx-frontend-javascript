3c86db95725bb1707531f6e777be1bb0be29ebe0
odongo-r <ronaldodongo76@gmail.com>
2024-08-05 15:44:28 +0300

    TAsk 0 and README.md update

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,28 +1,15 @@
-/**
- * Returns a string indicating a preference for const.
- * @returns {string} A string indicating a preference for const.
- */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-/**
- * Returns a string ' is okay'.
- * @returns {string} A string ' is okay'.
- */
 export function getLast() {
   return ' is okay';
 }
 
-/**
- * Combines two strings about variable declarations.
- * @returns {string} A combined string about using let and const.
- */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
 }
-
